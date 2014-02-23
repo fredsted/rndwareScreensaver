9e5397f65c7c4a167a02e2931fe86c8a52aa4827
Simon <simon@fredsted.me>
2014-02-23 19:59:40 +0100

    Add lazarus project files

---
+++ unit1.pas
@@ -0,0 +1,37 @@
+unit Unit1;
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+end.
+
